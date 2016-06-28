a9dedb4db64e26261558e6f8fffca168e0d8ab8c
Javier Portillo <javiermarquezportillo@gmail.com>
2016-06-27 22:46:51 -0600

    Añade test para probar que Suma no acepte datos no numéricos

---
+++ tests/SumaTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SumaTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->suma = new Suma;
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSumaTiraExcepcionConValoresNoNumericos()
+    {
+        $this->suma->calcular(falso, 'test');
+    }
+}
