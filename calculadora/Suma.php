6d63fcb8fe3b4dd1131a36c21a8837c9c834e71f
Javier Portillo <javiermarquezportillo@gmail.com>
2016-06-29 15:24:17 -0600

    Suma aprueba test: tirar excepcion al faltar argumentos

--- calculadora/Suma.php
+++ calculadora/Suma.php
@@ -2,8 +2,13 @@
 
 class Suma
 {
-    public function calcular($sumandoA, $sumandoB)
+    public function calcular($sumandoA = null, $sumandoB = null)
     {
+        if (isset($sumandoA) !== true || isset($sumandoB) !== true) {
+            throw new BadMethodCallException("Suma espera dos argumentos con valores numéricos");
+
+        }
+
         if (is_numeric($sumandoA) !== true || is_numeric($sumandoB) !== true) {
             throw new InvalidArgumentException("Suma espera dos argumentos con valores numéricos");
         }
